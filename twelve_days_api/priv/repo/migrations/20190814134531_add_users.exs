f85b2639fb7380f9a52700c91dad7c0c9756a26d
David McNight <david.mcnight@MacBook-Pro-6.local>
2019-08-14 12:39:01 -0400

    phx

--- twelve_days_api/priv/repo/migrations/20190814134531_add_users.exs
+++ twelve_days_api/priv/repo/migrations/20190814134531_add_users.exs
@@ -2,13 +2,10 @@ defmodule TwelveDaysApi.Repo.Migrations.AddUsers do
   use Ecto.Migration
 
   def change do
-    add :active, :boolean
-    add :email, :string, null: false
-    add :first_name, :string
-    add :last_name, :string
-    add :date_of_birth, :date
-    add :latest_activity, :naive_datetime
-    add :password_hash, :string
-    timestamps()
+    create table(:user) do
+      add :email, :string, null: false
+      add :latest_activity, :naive_datetime
+      add :password_hash, :string
+      timestamps()
   end
 end
