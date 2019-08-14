726ad135526c7879275d428fe53bb763797f0aac
David McNight <david.mcnight@MacBook-Pro-6.local>
2019-08-14 14:54:23 -0400

    User Schema

--- twelve_days_api/lib/twelve_days_api/users/user.ex
+++ twelve_days_api/lib/twelve_days_api/users/user.ex
@@ -2,6 +2,18 @@ defmodule TwelveDaysApi.Users.User do
   use TwelveDaysApi.Schema
   import Ecto.Changeset
 
+
+  @cast_attrs ~w(
+    email
+    last_name
+    latest_activity
+    managed
+    netsuite_id
+    password
+    password_confirmation
+  )a
+
+
   schema "users" do
     field :email, :string
     field :first_name, :string
@@ -11,9 +23,10 @@ defmodule TwelveDaysApi.Users.User do
     timestamps()
   end
 
-  # @spec changeset(User.t(), map) :: Ecto.Changeset.t()
-  def changeset(user) do
+  @spec changeset(User.t(), map) :: Ecto.Changeset.t()
+  def changeset(user, attrs) do
     user
+    |> cast(attrs, @cast_attrs)
     |> validate_required([:email])
     |> update_change(:email, &String.downcase/1)
     |> validate_format(:email, ~r/@/)
@@ -21,13 +34,13 @@ defmodule TwelveDaysApi.Users.User do
     |> put_password_hash()
   end
 
+  @spec put_password_hash(Ecto.Changeset.t()) :: Ecto.Changeset.t()
   defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
     changeset
     |> put_change(:password_digest, Bcrypt.hash_pwd_salt(password))
   end
 
   defp put_password_hash(%Ecto.Changeset{} = changeset), do: changeset
-
 
 
 
